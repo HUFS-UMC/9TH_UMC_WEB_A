c7372a462d920a1b495fe7a129733094f64b6aba
Lee SeungJoon <sj020521@naver.com>
2025-09-24 00:38:28 +0900

    2주차과제

---
+++ chapter2-2/src/useContext/ThemeContent.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import { Theme, useTheme } from "./context/ThemeProvider";
+
+export default function ThemeContent() {
+  const { theme, toggleTheme } = useTheme();
+
+  const isLightMode = theme === THEME.LIGHT;
+
+  return (
+    <div
+      className={clsx(
+        "p-4 h-dvh w-full",
+        isLightMode ? "bg-white" : "bg-gray-800"
+      )}
+    >
+      ThemeContent
+    </div>
+  );
+}
