58a885c2aa37a3b0282d750b643d796cf1319e2d
gminnggggg <bkm04290@naver.com>
2025-09-30 20:31:14 +0900

    week3

---
+++ week3_2/vite-project/src/components/MovieCard.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import type { Movie } from '../types/Movie';
+
+interface MovieCardProps {
+    movie: Movie;
+}
+
+export default function MovieCard({ movie } : MovieCardProps){
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+    console.log(movie.poster_path);
+
+
+    return (
+        <div 
+        className='relative'
+        onMouseEnter={() : void => setIsHovered(true)}
+        onMouseLeave={() : void => setIsHovered(false)}
+        >
+        <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={`${movie.title} 영화의 이미지`}
+                className=''
+        />
+
+        {isHovered && (
+            <div className='absolute inset-0 bg-gradient-to-t from-black/50
+            to-transparent backdrop-blur-md flex flex-col justify-center
+            items-center text-white'>
+                <h2>{movie.title}</h2>
+                <p>{movie.overview}</p>
+        </div>
+    )}
+    </div>
+    );
+}
