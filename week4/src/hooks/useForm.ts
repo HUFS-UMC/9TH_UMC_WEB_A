79f6ad82332260444a0239948d3ba9efa9376257
soeun0127 <leesoeun3895@naver.com>
2025-10-12 01:56:05 +0900

    week4 mission 2

---
+++ week4/src/hooks/useForm.ts
@@ -0,0 +1,46 @@
+import { useEffect, useReducer, useState, type ChangeEvent } from "react";
+
+interface UseFormProps<T> {
+    initialValue: T;
+    //값이 올바른지 검증하는 함수
+    validate: (values: T) => Record<keyof Text, string>;
+
+}
+
+function useForm<T>({initialValue, validate}: UseFormProps<T>) {
+    const [values, setValues] = useState(initialValue);
+    const [touched, setTouched] = useState<Record<string, boolean>>(); //key값은 string, value값은 boolean
+    const [errors, setErrors] = useState<Record<string, string>>();
+
+    //사용자가 입력값을 바꿀 때 실행되는 함수다.
+    const handleChange = (name:keyof T, text:string) => {
+        setValues({
+            ...values, //불변성 유지(기존 값 유지)
+            [name]: text,
+        });
+    };
+
+    const handleBlur = (name:keyof T) => {
+        setTouched({
+            ...touched, //불변성 유지(기존 값 유지)
+            [name]: true,
+        });
+    };
+
+    const getInputProps = (name: keyof T) => {
+        const value : T[keyof T] = values[name];
+
+        const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => handleChange(name, e.target.value);
+
+        const onBlur = () => handleBlur(name);
+
+        return {value, onChange, onBlur}
+    };
+
+    useEffect(() => {
+        const newErrors = validate(values);
+        setErrors(newErrors); //오류 메시지 업뎃
+    }, [validate, values]);
+
+    return { values, errors, touched, getInputProps };
+}
---
+++ week4/src/pages/LoginPage.tsx
@@ -0,0 +1,55 @@
+import {useState} from "react";
+import useForm from "../hooks/useForm"
+import { validateSignin, type UserSigninInformation } from "../utils/validate";
+
+const LoginPage = () => {
+    const {} = useForm<UserSigninInformation>({
+        initialValue: {
+            email: "",
+            password: "",
+        },
+        validate: validateSignin,
+    });
+
+    const handleSubmit = () => {
+        console.log(values);
+    };
+
+    const isDisabled = Object.values(errors || {}).some((error) => error.length > 0) || Object.values(values).some((value) => value === "");
+// 오류가 있으면 True
+//입력값이 없으면 True
+       
+    return (
+        <div className="flex flex-col items-center justify-center h-full gap-4">
+            <div className="flex flex-col gap-3">
+                <input
+                    {...getInputProps("email")}
+                    name="email"
+                    className="border border-[#ccc] w-[300px] p-[10px] focus: border-[#807bff] rounded-sm"
+                    type={"email"}
+                    placeholder={"이메일"}
+                />
+                {errors?.email && touched?.email && (
+                    <div className="text-red-500 text-sm">{errors.email}</div>
+                )}
+                <input 
+                    {...getInputProps("password")}
+                    className="border border-[#ccc] w-[300px] p-[10px] focus: border-[#807bff] rounded-sm"
+                    type={"password"}
+                    placeholder={"비밀번호"}
+                />
+                {errors?.password && touched?.password && (
+                    <div className="text-red-500 text-sm">{errors.password}</div>
+                )}
+                <button 
+                type='button' 
+                onClick={handleSubmit} 
+                disabled={isDisabled} 
+                className="w-full px-6 py-3 bg-blue-500 text-white font-semibold rounded-xl shadow-lg hover:bg-blue-600 transition-all duration-300 disabled:bg-gray-300 active:brightness-50 cursor-pointer">로그인</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginPage;
