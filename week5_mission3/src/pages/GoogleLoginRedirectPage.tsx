bf65909b0f7c9d66aace479bd47c94f662267e39
soeun0127 <leesoeun3895@naver.com>
2025-11-02 22:00:00 +0900

    5주차 미션3

---
+++ week5_mission3/src/pages/GoogleLoginRedirectPage.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+
+const GoogleLoginRedirectPage = () => {
+    const {setItem: setAccessToken} = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+    const {setItem: setRefreshToken} = useLocalStorage(LOCAL_STORAGE_KEY.refreshToken);
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const accessToken = urlParams.get(LOCAL_STORAGE_KEY.accessToken);
+        const refreshToken = urlParams.get(LOCAL_STORAGE_KEY.refreshToken);
+
+        if(accessToken){
+            setAccessToken(accessToken);
+            setRefreshToken(refreshToken);
+            window.location.href = '/my';
+        }
+
+        //urlParams는 4개의 query parameter인 id, name, access, refresh를 의미
+        //window.loation.search는 urlParams 뒤의 query parameter를 의미
+    }, [setAccessToken, setRefreshToken]);
+    return (
+        <div>
+            구글 로그인 리다이렉 화면
+        </div>
+    )
+};
+
+export default GoogleLoginRedirectPage;
