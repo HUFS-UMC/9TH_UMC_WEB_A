6d386dedf575a3f3ad76b279bc83f8cd3d507060
Lee SeungJoon <sj020521@naver.com>
2025-11-03 14:04:24 +0900

    5주차 과제입니다.

---
+++ UMC-9th-mission-fe/src/App.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> }, // index: true는 path: "/"를 의미. 홈경로일때 13번째 줄이 렌더링
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
