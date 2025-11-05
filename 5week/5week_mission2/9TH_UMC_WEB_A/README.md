9bbbe68caa70a692adf3a414eac9ec7a70b85cfb
손성원 <sonseongwon@sonseong-won-ui-MacBookPro.local>
2025-11-05 22:45:34 +0900

    6주차 미션 수행

--- 5week/5week_mission2/9TH_UMC_WEB_A/README.md
+++
@@ -1,2 +0,0 @@
-# 9TH_UMC_WEB_A
-UMC 9기 WEB_A
--- 5week/5week_mission3/src/pages/NotFoundPage.tsx
+++
@@ -1,9 +0,0 @@
-const NotFoundPage = () => {
-    return (
-        <div>
-            낫파운드 페이지
-        </div>
-    );
-};
-
-export default NotFoundPage;
