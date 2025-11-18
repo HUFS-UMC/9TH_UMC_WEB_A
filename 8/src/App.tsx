b65d516b43143476650103c6ec089d793c445e77
Lee SeungJoon <sj020521@naver.com>
2025-11-18 18:57:30 +0900

    8주차 과제 제출합니다.

---
+++ 8/src/App.tsx
@@ -0,0 +1,76 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  type RouteObject,
+} from "react-router-dom";
+import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage";
+import LoginPage from "./pages/LoginPage";
+import HomeLayout from "./layouts/HomeLayout";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import MyPage from "./pages/MyPage";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import LpDetailPage from "./pages/LpDetailPage";
+import ThrottlePage from "./pages/ThrottlePage";
+
+// 1. 홈페이지
+// 2. 로그인 페이지
+// 3. 회원가입 페이지
+
+// publicRoutes : 인증 없이 접근 가능
+const publicRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> }, // index: true는 path: "/"를 의미. 홈경로일때 13번째 줄이 렌더링
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignupPage /> },
+      { path: "v1/auth/google/callback", element: <GoogleLoginRedirectPage /> },
+      { path: "lps/:lpId", element: <LpDetailPage /> },
+      { path: "throttle", element: <ThrottlePage /> },
+    ],
+  },
+];
+
+// protectedRoutes : 인증 필요
+const protectedRoutes: RouteObject[] = [
+  {
+    path: "/",
+    element: <ProtectedLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/my",
+        element: <MyPage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
+}
